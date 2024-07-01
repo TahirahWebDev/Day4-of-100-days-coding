cf6f683e31ac76718be2267e3a728e95c24e23be
Tahirah Roohi <157677736+TahirahWebDev@users.noreply.github.com>
2024-07-01 07:12:49 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+"use strict";
+// Question 10
+let myName = "Tahirah";
+console.log(`Hello ${myName}, Would you like to learn sme Typescript today?`);
+// Question 11
+let names = ["Tahirah", "Tooba", "Mahi"];
+for (let i = 0; i < names.length; i++) {
+    console.log(names[i]);
+}
+// Question 12
+let names2 = ["Tahirah", "Tooba", "Mahi"];
+for (let i = 0; i < names2.length; i++) {
+    console.log(`Hello ${names2[i]}`);
+}
---
+++ main.ts
@@ -0,0 +1,20 @@
+// Question 10
+
+let myName:string = "Tahirah";
+console.log(`Hello ${myName}, Would you like to learn sme Typescript today?`);
+
+
+// Question 11
+
+let names:string[] = ["Tahirah", "Tooba", "Mahi"];
+for (let i = 0; i < names.length; i++){
+    console.log(names[i])
+}
+
+
+// Question 12
+
+let names2:string[] = ["Tahirah", "Tooba", "Mahi"];
+for (let i = 0; i < names2.length; i++){
+    console.log(`Hello ${names2[i]}`)
+}
